3db230530d03ab2069d96a114916b5cfb4c52f14
Chris Bergquist <chris.bergquist@jask.com>
2019-02-03 15:53:07 -0600

    initial commit of Org config and base init.el

---
+++ init.el
@@ -0,0 +1,12 @@
+(require 'org)
+
+(defun tangle-init ()
+  "If the current buffer is 'init.org' the code-blocks are tangled, and the tangled file is compiled."
+  (when (equal (buffer-file-name)
+               (expand-file-name (concat user-emacs-directory "init.org")))
+    ;; Avoid running hooks when tangling
+    (let ((prog-mode-hook nil))
+      (org-babel-tangle)
+      (byte-compile-file (concat user-emacs-directory "init.el")))))
+
+(add-hook 'after-save-hook #'tangle-init)
